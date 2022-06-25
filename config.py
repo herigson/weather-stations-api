641b00300cd62d8b973acc1e8fd2fc557a0d3cd5
herigson <felipevbraga@outlook.com>
2022-06-24 22:51:39 -0300

    First Commit.

--- config.py
+++ config.py
@@ -1,11 +1,1 @@
-from instance import config
-
-# Declaração para habilitar o ambiente de desenvolvimento
-DEBUG = True
-
-# Definição do Banco de Dados MySQL
-SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{config.user}:{config.password}@{config.host}/{config.db}'
-
-DATABASE_CONNECT_OPTIONS = {}
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+api_key = "807bcfaf"
